b9ec8f56081428dd8e379aee517fcb169afa10e6
Quynh Tran <nhim0406@Quynhs-MBP.home>
2016-02-26 21:24:25 +0000

    added viewcontroller.swift files

---
+++ sacinhimtest/AgeViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  sacinhimtest
+//
+//  Created by Quynh Tran on 26/02/2016.
+//  Copyright © 2016 Quynh. All rights reserved.
+//
+
+import UIKit
+
+class AgeViewController: UIViewController {
+    
+    //Link your UI elements here
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        //position and setting before animation here
+        
+        
+        
+        
+        
+    } // end of viewWillAppear
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        //animations here
+        
+        
+        
+        
+        
+    } // end of viewDisAppear
+    
+} //end of class
---
+++ sacinhimtest/ContactViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  sacinhimtest
+//
+//  Created by Quynh Tran on 26/02/2016.
+//  Copyright © 2016 Quynh. All rights reserved.
+//
+
+import UIKit
+
+class ContactViewController: UIViewController {
+    
+    //Link your UI elements here
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        //position and setting before animation here
+        
+        
+        
+        
+    } // end of viewWillAppear
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        //animations here
+        
+        
+        
+        
+    } // end of viewDisAppear
+    
+} //end of class
